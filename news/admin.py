1fe63c7c7d2172bebde080637057ded93539b7ce
nadirkorkut <korkutnadir@outlook.com>
2020-04-02 13:07:10 +0300

    Model Category And News Relationship

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from news.models import Category
+from news.models import Category, News
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title','status']
     list_filter = ['status']
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title','status']
+    list_filter = ['status','category']
+
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(News,NewsAdmin)
