aabe89921e806136aa53c73b494b481c6b55736c
nadirkorkut <korkutnadir@outlook.com>
2020-04-14 20:08:50 +0300

    Video 7 Finished-News Admin Image Gallery

--- home/views.py
+++ home/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from home.models import Setting
+
+
 def index(request):
-    text="Merhaba Django"
-    context={"Text" : text}
+    setting = Setting.objects.get(pk=1)
+    context={"setting" : setting}
     return render(request, 'index.html', context)
