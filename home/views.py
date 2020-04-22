dff7261d557be415dd869a1df686b83993483447
nadirkorkut <korkutnadir@outlook.com>
2020-04-22 21:15:30 +0300

    Video 13 Finished-Contact Form

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from home.models import Setting
+from home.models import Setting, ContactFormu,ContactFormMessage
 
 
 def index(request):
@@ -21,6 +22,19 @@ def referanslar(request):
     return render(request, 'referanslarimiz.html', context)
 
 def iletisim(request):
+    if request.method == 'POST':
+        form=ContactFormu(request.POST)
+        if form.is_valid():
+            data=ContactFormMessage()
+            data.name=  form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request,"Mesajınız Başarılı bir şekilde iletilmiştir.Teşekkür Ederiz.")
+            return HttpResponseRedirect('/iletisim')
     setting = Setting.objects.get(pk=1)
-    context={"setting" : setting}
-    return render(request, 'iletişim.html', context)
+    form=ContactFormu()
+    context={"setting" : setting,'form' : form}
+    return render(request, 'iletisim.html', context)
