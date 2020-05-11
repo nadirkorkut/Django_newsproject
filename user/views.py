8493a9654354d54fe8f324a77990fbda2fc73bc1
nadirkorkut <korkutnadir@outlook.com>
2020-05-11 16:31:10 +0300

    List And Delete User Comments

--- user/views.py
+++ user/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,HttpResponseRedirect
-from news.models import Category
+from news.models import Category,Comment
 from user.models import UserProfile
 from user.forms import UserUpdateForm,ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
@@ -59,6 +61,26 @@ def change_password(request):
             'form' : form,
             'category' : category
         })
+
+
+@login_required(login_url='/login') #Check Login
+def comments(request):
+    category = Category.objects.all()
+    current_user =request.user
+    comments = Comment.objects.filter(user_id=current_user.id)
+    context = {
+        'category' : category,
+        'comments' : comments,
+    }
+    return render(request, 'user_comments.html', context)
+
+
+@login_required(login_url='/login') #Check Login
+def deletecomment(request,id):
+    current_user = request.user
+    Comment.objects.filter(id=id , user_id=current_user.id).delete()
+    messages.success(request, 'Comment Deleted...')
+    return HttpResponseRedirect('/user/comments')
 
 
 
